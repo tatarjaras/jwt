119e569d603b68b52e9c5370221e217e0ac4b41c
tatarjaras <havasim@kkszki.hu>
2024-12-18 11:21:06 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
-
-import {BrowserRouter as Router, Routes, Route,NavLink} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
 import {Login} from './Login';
-import {SzallasList} from './SzallasList';
 import {Logout} from './Logout';
+import {SzallasList} from './SzallasList'; 
 import './App.css';
 
-export default function App() {
+export const App = () => {
   return (
     <Router>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+        <div className='container-fluid'>
+        <div className='navbar-brand'>SzallasJWT</div>
+        <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-expanded="false" aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id="navbarNav">
         <ul className="navbar-nav">
-          <li className="navbar-item"><NavLink to="/">Login</NavLink></li>
-          
-          <li className="navbar-item"><NavLink to="/SzallasList">SzallasList</NavLink></li>
+          <li className='nav-item'>
+            <NavLink to={"/"} className={({isActive}) => "nav-link" + (isActive ? " active" : "")}><span className='nav-link'>Login</span></NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink to={"/SzallasList"} className={({isActive}) => "nav-link" + (isActive ? " active" : "")}><span className='nav-link'>SzallasList</span></NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink to={"/Logout"} className={({isActive}) => "nav-link" + (isActive ? " active" : "")}><span className='nav-link'>Kijelentkezés</span></NavLink>
+          </li>
         </ul>
-      </nav><li className="navbar-item"><NavLink to="/Logout">kijelentkezés</NavLink></li>
+        </div>
+        </div>
+      </nav>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/SzallasList" element={<SzallasList />} />
-        <Route path="/Logout" element={<Logout />} />
+        <Route path="/" element = {<Login />} />
+        <Route path="SzallasList" element = {<SzallasList />} />
+        <Route path="/Logout" element = {<Logout />} />
       </Routes>
     </Router>
   );
